50f4ea414520c9586539f0ba2cec8b0cfa53fbb4
Nyrolla <alloryn.mappin@me.com>
2023-09-28 11:05:06 -0600

    Added Birthday to sign up sheet

--- Models/Login.cs
+++ Models/Login.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace Assignment1v3.Models
 {
@@ -6,19 +7,31 @@ namespace Assignment1v3.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "First Name")]
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string Name_First { get; set; } = string.Empty;
 
+        [Display(Name = "Last Name")]
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string Name_Last { get; set; } = string.Empty;
 
+        [Display(Name = "Email Address")]
         [DataType(DataType.EmailAddress)]
         [Required]
         public string Email_Username { get; set; } = string.Empty;
 
         [Required]
         public string Password { get; set; } = string.Empty;
+
+        [Display(Name = "Date of Birth")]
+        [BindProperty]
+        [DataType(DataType.Date)]
+        [MinimumAge(16)] //Set the minimum age to 16 years old
+        public DateTime Birth_Date { get; set; }
+
+
+
     }
 }
