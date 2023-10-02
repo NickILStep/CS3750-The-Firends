e2943d7f1bb0ccbaead05019c6562a2ef5f65436
Gavin Evans <oblivion.industries.gman@gmail.com>
2023-10-02 11:19:32 -0600

    Added Courses and Fixed it (Maybe)
    Added a Courses link to the navbar and implemented almost all the necessary features. This is happening because I want to test something.

---
+++ Models/Schools.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment1v3.Models
+{
+    public enum Schools
+    {
+        [Display(Name = "Computer Science")]
+        CS,
+
+        [Display(Name = "Electric Engineering")]
+        EE,
+
+        [Display(Name = "Math")]
+        MATH,
+
+        [Display(Name = "Data Science")]
+        DS,
+
+        [Display(Name = "Pyrotechnics")]
+        PT
+    }
+}
