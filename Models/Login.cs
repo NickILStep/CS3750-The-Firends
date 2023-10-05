6fd430b522b1e8399ba09adaf4aa3a24d80cd104
Nyrolla <alloryn.mappin@me.com>
2023-10-05 16:12:41 -0600

    Add error message if confirm password does not match first password

--- Models/Login.cs
+++ Models/Login.cs
@@ -33,7 +33,7 @@ namespace Assignment1v3.Models
         [DataType(DataType.Password)] // This makes the input field a password input
         public string Password { get; set; }
 
-        [Display(Name = "ConfirmPassword")]
+        [Display(Name = "Confirm Password")]
         [Required]
         [DataType(DataType.Password)] // This makes the input field a password input
         public string ConfirmPassword { get; set; }
