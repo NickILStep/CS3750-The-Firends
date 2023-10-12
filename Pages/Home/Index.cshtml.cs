8e62f4e00a873ce94200b94bac6a9fda326fe1b7
Nyrolla <alloryn.mappin@me.com>
2023-10-12 09:13:04 -0600

    Cleaned up Dashboard and added features: Calls it Dashbaord Added bones of TO-DO list cards Created links on cards

--- Pages/Home/Index.cshtml.cs
+++ Pages/Home/Index.cshtml.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Assignment1v3.Data;
 using Assignment1v3.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Assignment1v3.Pages.Home
 {
+    //[Authorize(Policy = "MustBeStudent")] // Add this attribute to restrict access
     public class IndexModel : PageModel
     {
         private readonly Assignment1v3.Data.Assignment1v3Context _context;
@@ -19,7 +21,7 @@ namespace Assignment1v3.Pages.Home
             _context = context;
         }
 
-        public IList<Course> Course { get;set; } = default!;
+        public IList<Course> Course { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
