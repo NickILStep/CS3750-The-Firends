c46b3ec3609d6469eb1ddeb912c7de55454267a6
rockstar12410 <austinallred5@mail.weber.edu>
2023-10-12 19:11:56 -0600

    Registration Commit

---
+++ Models/StudSched.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment1v3.Models
+{
+    public class StudSched
+    {
+        public int Id { get; set; }
+
+        public string Email_Username { get; set; }
+
+        public int CourseNum { get; set; }
+    }
+}
---
+++ Pages/Registrations/Index.cshtml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Assignment1v3.Data;
+using Assignment1v3.Models;
+
+namespace Assignment1v3.Pages.Registrations
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Assignment1v3.Data.Assignment1v3Context _context;
+        public IndexModel(Assignment1v3.Data.Assignment1v3Context context)
+        {
+            _context = context;
+        }
+
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
+        public IList<Course> Course { get; set; } = default!;
+
+        public IList<StudSched> StudSched { get; set; }
+
+        public async Task OnGetAsync(string sortOrder,  string searchString)
+        {
+            // using System;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            CurrentFilter = searchString;
+            //Course = await _context.Course.ToListAsync();
+
+            IQueryable<Course> coursesIQ = from s in _context.Course
+                                             select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                coursesIQ = coursesIQ.Where(s => s.CourseName.Contains(searchString)
+                                       || s.Description.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    coursesIQ = coursesIQ.OrderByDescending(s => s.CourseName);
+                    break;
+                case "Date":
+                    coursesIQ = coursesIQ.OrderBy(s => s.StartTime);
+                    break;
+                case "date_desc":
+                    coursesIQ = coursesIQ.OrderByDescending(s => s.StartTime);
+                    break;
+                default:
+                    coursesIQ = coursesIQ.OrderBy(s => s.CourseNumber);
+                    break;
+            }
+
+            Course = await coursesIQ.AsNoTracking().ToListAsync();
+        }
+
+    }
+}
