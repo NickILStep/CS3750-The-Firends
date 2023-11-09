03487a9d3d45b77330911269694f6a60282c95ea
Nyrolla <alloryn.mappin@me.com>
2023-11-09 09:33:00 -0700

    Now displays courses students are taking. Started rolling on the drop courses

---
+++ Pages/Home/DropCourse.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Assignment1v3.Data;
+using Assignment1v3.Models;
+
+namespace Assignment1v3.Pages.Home
+{
+    public class DropCourseModel : PageModel
+    {
+        private readonly Assignment1v3Context _context;
+
+        public DropCourseModel(Assignment1v3Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int courseId)
+        {
+            Course = await _context.Course.FirstOrDefaultAsync(c => c.Id == courseId);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var courseToDrop = await _context.Course.FindAsync(CourseId);
+
+            if (courseToDrop != null)
+            {
+                // Remove the course from the database
+                _context.Course.Remove(courseToDrop);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage();
+        }
+    }
+}
--- Pages/Home/StudentDashboard.cshtml.cs
+++ Pages/Home/StudentDashboard.cshtml.cs
@@ -23,10 +23,11 @@ namespace Assignment1v3.Pages.Home
             _context = context;
         }
 
-        public IList<StudentCourseViewModel> Course { get; set; }
+        public List<Course> Course { get; set; }
+
         public async Task OnGetAsync()
         {
-            Course = new List<StudentCourseViewModel>(); // Initialize the list of courses
+            Course = new List<Course>();
 
             // Get the currently authenticated user's email address claim
             var userEmailClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email);
@@ -34,39 +35,25 @@ namespace Assignment1v3.Pages.Home
             if (userEmailClaim != null)
             {
                 var userEmailClaimValue = userEmailClaim.Value;
+                var studentCourses = await _context.StudSched.Where(x => x.Email_Username.Contains(userEmailClaimValue)).ToListAsync();
+                System.Diagnostics.Debug.WriteLine(studentCourses);
 
-                // Use a regular expression to extract the email address
-                var match = Regex.Match(userEmailClaimValue, ":(.*)$"); // Match everything after the colon
-                if (match.Success)
+                foreach (var tempcourse in studentCourses)
                 {
-                    // Extract the email address part and trim any leading or trailing spaces
-                    var userEmail = match.Groups[1].Value.Trim();
+                    var matchingCourses = _context.Course.Where(x => x.CourseNumber == tempcourse.CourseNum).ToList();
 
-                    // Use userEmail to query the database for the student's courses
-                    Course = await _context.Login
-                        .Where(u => u.Email_Username == userEmail)
-                        .Join(
-                            _context.StudSched,
-                            user => user.Email_Username,
-                            studSched => studSched.Email_Username,
-                            (user, studSched) => new { User = user, CourseNum = studSched.CourseNum }
-                        )
-                        .Join(
-                            _context.Course,
-                            combined => combined.CourseNum,
-                            course => course.CourseNumber,
-                            (combined, course) => new StudentCourseViewModel
-                            {
-                                UserFirstName = combined.User.Name_First,
-                                UserLastName = combined.User.Name_Last,
-                                CourseName = course.CourseName,
-                                Location = course.Location,
-                                StartTime = course.StartTime
-                            }
-                        )
-                        .ToListAsync();
+                    if (matchingCourses.Count > 0)
+                    {
+                        Course myCourse = matchingCourses[0];
+                        Course.Add(myCourse);
+                    }
+
                 }
+
+                System.Diagnostics.Debug.WriteLine(Course);
+
             }
         }
+
     }
 }
